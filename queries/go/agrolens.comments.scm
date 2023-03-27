93b8fe9e132b7decd98d4ad3d70a67e021e01051
Kim Gert Nielsen <kgn@one.com>
2023-03-27 19:29:33 +0200

    Better example for go + comments

---
+++ queries/go/agrolens.comments.scm
@@ -0,0 +1,1 @@
+(comment) @agrolens.scope
--- tests/go_spec.lua
+++ tests/go_spec.lua
@@ -1,5 +1,4 @@
 describe("go", function()
-
     local lens = nil
     local buffers = nil
 
@@ -9,29 +8,38 @@ describe("go", function()
         assert.equal(#buffers, 1)
 
         lens = require("telescope._extensions.agrolenslib")
-        lens._get_captures({queries={"functions"}, bufids=buffers})
+        lens._get_captures({queries = {"functions"}, bufids = buffers})
     end)
 
     it("functions", function()
-        local entries = lens._get_captures({queries={"functions"}, bufids=buffers})
+        local entries = lens._get_captures({queries = {"functions"}, bufids = buffers})
 
-        -- We should have found 3 functions
-        assert.equals(#entries, 3)
+        assert.equals(#entries, 4)
 
-        assert.equals("tests/go/main.go:9:0:func (p *Person) GetAge() int {", entries[1])
-        assert.equals("tests/go/main.go:13:0:func ret42() int {", entries[2])
-        assert.equals("tests/go/main.go:17:0:func main() {", entries[3])
+        assert.equals("tests/go/main.go:15:0:func (p *Person) GetName() string {", entries[1])
+        assert.equals("tests/go/main.go:19:0:func print_person(p Person, fn format_func) {", entries[2])
+        assert.equals("tests/go/main.go:23:0:func main() {", entries[3])
+        assert.equals("tests/go/main.go:29:1:\tformat_func := func(p Person) string {", entries[4])
     end)
 
     it("callings", function()
-        local entries = lens._get_captures({queries={"callings"}, bufids=buffers})
+        local entries = lens._get_captures({queries = {"callings"}, bufids = buffers})
 
-        assert.equals(#entries, 5)
+        assert.equals(#entries, 10)
 
-        assert.equals("tests/go/main.go:18:1:\tnum := ret42()", entries[1])
-        assert.equals("tests/go/main.go:21:10:\tnum2 := p.GetAge()", entries[2])
-        assert.equals("tests/go/main.go:23:1:\tret42()", entries[3])
-        assert.equals("tests/go/main.go:24:2:\tp.GetAge()", entries[4])
-        assert.equals("tests/go/main.go:26:2:\tfmt.Println(num, num2)", entries[5])
+        assert.equals("tests/go/main.go:20:2:\tfmt.Println(fn(p))", entries[1])
+        assert.equals("tests/go/main.go:20:1:\tfmt.Println(fn(p))", entries[2])
+        assert.equals('tests/go/main.go:30:15:\t\ttt, err := time.Parse("01-02-2006", p.Born)', entries[3])
+        assert.equals("tests/go/main.go:32:13:\t\t\treturn err.Error()", entries[4])
+        assert.equals("tests/go/main.go:35:19:\t\tcurrentTime := time.Now()", entries[5])
+    end)
+
+    it("comments", function()
+        local entries = lens._get_captures({queries = {"comments"}, bufids = buffers})
+
+        assert.equals(#entries, 2)
+
+        assert.equals("tests/go/main.go:26:1:\t/*", entries[1])
+        assert.equals("tests/go/main.go:41:1:\t// Print Donald", entries[2])
     end)
 end)
