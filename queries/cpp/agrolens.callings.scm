940c240ea0e4a758b78379e3b796e55b7fafd796
Kim Gert Nielsen <kgn@one.com>
2023-03-27 14:02:15 +0200

    Rewrote cpp example and fixed callings + added comments

--- queries/cpp/agrolens.callings.scm
+++ queries/cpp/agrolens.callings.scm
@@ -24,3 +24,9 @@
         function:(identifier) @agrolens.name)) @agrolens.scope
 
 (declaration declarator:(init_declarator value:(call_expression function:(identifier) @agrolens.name))) @agrolens.scope
+
+(expression_statement(assignment_expression right:(call_expression function:(identifier) @agrolens.name))) @agrolens.scope
+
+(expression_statement(binary_expression left:(binary_expression right:(call_expression function:(identifier) @agrolens.name)))) @agrolens.scope
+
+(declaration declarator:(init_declarator value:(call_expression function:(parenthesized_expression) @agrolens.name))) @agrolens.scope
--- tests/cpp/main.cc
+++ tests/cpp/main.cc
@@ -1,27 +1,70 @@
 #include <iostream>
+#include <sstream>
+
+typedef int (*conv_func)(time_t, time_t);
 
 class Person {
 public:
-  int age;
+  Person(std::string name, std::string born);
+  friend std::ostream &operator<<(std::ostream &out, const Person &p);
+  friend int days_since_birth(const Person &p, conv_func func);
 
-  Person(int age) : age(age) {}
-  int getAge() { return age; }
-  void setAge(int age);
+private:
+  std::string name;
+  std::string born;
+  int age_in_days;
 };
 
-void Person::setAge(int age) { this->age = age; }
+Person::Person(std::string name, std::string born) {
+  this->name = name;
+  this->born = born;
+}
 
-void someFunc() { std::cout << "ran function" << std::endl; }
+/* basic seconds to days
+*/
+int sec_to_days(time_t now, time_t born) { 
+  return (now - born) / (60 * 60 * 24); }
 
-int returnStuff() { return 42; }
+int days_since_birth(const Person &p, conv_func func) {
+
+  std::istringstream is(p.born);
+
+  int year, month, day;
+
+  char delimiter;
+  if (is >> day >> delimiter >> month >> delimiter >> year) {
+    time_t now = time(0);
+    struct tm *parsedDate;
+
+    parsedDate = localtime(&now);
+    parsedDate->tm_year = year - 1900;
+    parsedDate->tm_mon = month - 1;
+    parsedDate->tm_mday = day;
+
+    time_t born = mktime(parsedDate);
+
+    int age = (*func)(now, born);
+
+    return age;
+  }
+
+  return 0;
+}
+
+std::ostream &operator<<(std::ostream &out, const Person &p) {
+
+  out << p.name << " is " << days_since_birth(p, sec_to_days) << " days old";
+
+  return out;
+}
 
 int main() {
-  Person p(42);
 
-  std::cout << "Age: " << p.getAge() << std::endl;
-  p.getAge();
-  someFunc();
-  int i = returnStuff();
-  std::cout << i << std::endl;
+  // Create Donald
+  Person donald("Donald Duck", "07-09-1934");
+
+  // Print Donald
+  std::cout << donald << std::endl;
+
   return 0;
 }
